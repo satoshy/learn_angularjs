c2063b9a50066f0d876499774b719dc1e7c21aab
satoshy <alex.stipanenko@gmail.com>
2016-02-17 09:49:33 +0200

    added images

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,6 @@ phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
 	$http.get('phones/phones.json').success(function(data) {
 		$scope.phones = data;
 	});
-	
+
   $scope.orderProp = 'age';
 });
